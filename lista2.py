b73d7c46e1f479ba502b6037b2bbf94581e3fd87
Carlos Fernandes <57178244+carlosamfernandes@users.noreply.github.com>
2020-04-11 07:47:19 -0300

    Arrumando detalhes

--- lista2.py
+++ lista2.py
@@ -92,10 +92,5 @@ if letra in ("A", "E", "I", "O", "U"):
 else: 
   print("A letra digitada é uma consoante")
 
-"""Crie um algoritmo que calcule e mostre o novo valor de um salário a partir das seguintes regras: salários de até R$ 1.000,00 inclusive recebem 30 % 
-de aumento, salários de até R$ 2.000,00 inclusive recebem 25%, 
-salários de até R$ 3.000,00 inclusive recebem 20%, 
-salários de até R$ 4.000,00 inclusive recebem 15% 
-e salários acima de R$ 4.000,00 recebem apenas 10%.
-"""
+
 
